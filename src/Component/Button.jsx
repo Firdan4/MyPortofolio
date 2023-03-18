b546943480da69d574a3b74a732d7ad6acf80f9c
Firdan4 <herufirdan.alghovary@gmail.com>
2023-03-18 10:03:24 +0700

    perbaiki problem di componen button

--- src/Component/Button.jsx
+++ src/Component/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button1 = (props) => {
   return (
     <div>
-      <button className="hidden md:flex justify-center items-center items-center justify-center w-[120px] h-[40px] bg-orange-500 rounded-md hover:bg-slate-800 duration-200 text-white font-semibold">
+      <button className="hidden md:flex justify-center items-center w-[120px] h-[40px] bg-orange-500 rounded-md hover:bg-slate-800 duration-200 text-white font-semibold">
         {props.contactProps}
       </button>
     </div>
